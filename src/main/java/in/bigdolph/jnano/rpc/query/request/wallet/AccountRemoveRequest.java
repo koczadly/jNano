0b83cee0df4762a44164ac7ef016d8b8854734ae
Karl Oczadly <kocz@live.co.uk>
2018-03-03 03:02:57 +0000

    Add account remove request

---
+++ src/main/java/in/bigdolph/jnano/rpc/query/request/wallet/AccountRemoveRequest.java
@@ -0,0 +1,36 @@
+package in.bigdolph.jnano.rpc.query.request.wallet;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import in.bigdolph.jnano.rpc.query.request.RPCRequest;
+import in.bigdolph.jnano.rpc.query.response.specific.AccountRemoveResponse;
+import in.bigdolph.jnano.rpc.query.response.specific.NodeVersionResponse;
+
+public class AccountRemoveRequest extends RPCRequest<AccountRemoveResponse> {
+    
+    @Expose
+    @SerializedName("wallet")
+    private String walletId;
+    
+    @Expose
+    @SerializedName("account")
+    private String account;
+    
+    
+    public AccountRemoveRequest(String walletId, String account) {
+        super("account_remove", AccountRemoveResponse.class);
+        this.walletId = walletId;
+        this.account = account;
+    }
+    
+    
+    
+    public String getWalletId() {
+        return walletId;
+    }
+    
+    public String getAccount() {
+        return account;
+    }
+    
+}
---
+++ src/main/java/in/bigdolph/jnano/rpc/query/response/specific/AccountRemoveResponse.java
@@ -0,0 +1,19 @@
+package in.bigdolph.jnano.rpc.query.response.specific;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import in.bigdolph.jnano.rpc.query.response.RPCResponse;
+
+public class AccountRemoveResponse extends RPCResponse {
+
+    @Expose
+    @SerializedName("removed")
+    private int accountsRemoved;
+    
+    
+    
+    public int getAccountsRemoved() {
+        return accountsRemoved;
+    }
+    
+}
