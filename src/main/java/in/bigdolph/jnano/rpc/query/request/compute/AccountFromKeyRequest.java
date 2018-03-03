befc435a944bf514b2a9c75577d570ed8b7dd64c
Karl Oczadly <kocz@live.co.uk>
2018-03-03 02:34:01 +0000

    Add account get request

---
+++ src/main/java/in/bigdolph/jnano/rpc/query/request/compute/AccountFromKeyRequest.java
@@ -0,0 +1,26 @@
+package in.bigdolph.jnano.rpc.query.request.compute;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import in.bigdolph.jnano.rpc.query.request.RPCRequest;
+import in.bigdolph.jnano.rpc.query.response.generic.AccountResponse;
+import in.bigdolph.jnano.rpc.query.response.specific.NodeVersionResponse;
+
+public class AccountFromKeyRequest extends RPCRequest<AccountResponse> {
+    
+    @Expose
+    @SerializedName("key")
+    private String accountKey;
+    
+    
+    public AccountFromKeyRequest(String accountKey) {
+        super("account_get", AccountResponse.class);
+        this.accountKey = accountKey;
+    }
+    
+    
+    
+    public String getAccountKey() {
+        return accountKey;
+    }
+}
