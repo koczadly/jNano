664a9d01852d83fbe3cdc5c1075016f501cf0d7e
Karl Oczadly <kocz@live.co.uk>
2018-03-03 03:02:00 +0000

    Add account move request

---
+++ src/main/java/in/bigdolph/jnano/rpc/query/request/wallet/AccountMoveRequest.java
@@ -0,0 +1,45 @@
+package in.bigdolph.jnano.rpc.query.request.wallet;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import in.bigdolph.jnano.rpc.query.request.RPCRequest;
+import in.bigdolph.jnano.rpc.query.response.specific.AccountMoveResponse;
+import in.bigdolph.jnano.rpc.query.response.specific.NodeVersionResponse;
+
+public class AccountMoveRequest extends RPCRequest<AccountMoveResponse> {
+    
+    @Expose
+    @SerializedName("wallet")
+    private String destinationWalletId;
+    
+    @Expose
+    @SerializedName("source")
+    private String sourceWalletId;
+    
+    @Expose
+    @SerializedName("accounts")
+    private String[] accounts;
+    
+    
+    public AccountMoveRequest(String sourceWalletId, String destinationWalletId, String... accounts) {
+        super("account_move", AccountMoveResponse.class);
+        this.sourceWalletId = sourceWalletId;
+        this.destinationWalletId = destinationWalletId;
+        this.accounts = accounts;
+    }
+    
+    
+    
+    public String getSourceWalletId() {
+        return sourceWalletId;
+    }
+    
+    public String getDestinationWalletId() {
+        return destinationWalletId;
+    }
+    
+    public String[] getAccounts() {
+        return accounts;
+    }
+    
+}
