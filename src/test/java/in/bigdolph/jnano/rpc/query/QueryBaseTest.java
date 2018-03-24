e90ecbb911de5f299efafa602c728eaa823a518a
Karl Oczadly <kocz@live.co.uk>
2018-03-24 23:51:20 +0000

    Change test wallet ID

--- src/test/java/in/bigdolph/jnano/rpc/query/QueryBaseTest.java
+++ src/test/java/in/bigdolph/jnano/rpc/query/QueryBaseTest.java
@@ -14,7 +14,7 @@ public abstract class QueryBaseTest {
     public static final String TEST_ACCOUNT =       "xrb_365md53cciaquuerrp97psfrsg1bd4rw5d8cyfpyefgofsopiuzozibdnbmx";
     public static final String TEST_ACCOUNT_BLANK = "xrb_3jxpteeg47a8ofi5h91mazey9ggeas3xais387rejb5413gaicbax9bq9mgr";
     
-    public static final String TEST_WALLET =        "69F429CC16E115E0F6BE5698A3AAAA57318A967F45FC50A5E18E1304ED4D2D1C";
+    public static final String TEST_WALLET =        "B4ECF585D887B590907949C41F73BB11AA0BD4FD98563CC5D810EF26FAAD948E";
     
     public static final String TEST_BLOCK_SEND =    "652BA0461666145EEC3375389E85EE840B6C75CCB47EACB0F4DEDBD46F6BEB3C";
     public static final String TEST_BLOCK_RECEIVE = "85D771106D26D73B4C804E44BC9CFC158D147DF8D6F4B65B09FC046E353C6CB6";
