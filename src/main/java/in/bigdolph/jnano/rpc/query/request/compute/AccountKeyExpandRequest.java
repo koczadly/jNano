9536e5e8f055950ff07fe6cd638cbdbfe133cdc7
Karl Oczadly <kocz@live.co.uk>
2018-03-24 22:44:04 +0000

    Add account key expand request

---
+++ src/main/java/in/bigdolph/jnano/rpc/query/request/compute/AccountKeyExpandRequest.java
@@ -0,0 +1,27 @@
+package in.bigdolph.jnano.rpc.query.request.compute;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import in.bigdolph.jnano.rpc.query.request.RPCRequest;
+import in.bigdolph.jnano.rpc.query.response.generic.AccountKeyPairResponse;
+import in.bigdolph.jnano.rpc.query.response.specific.NodeVersionResponse;
+
+public class AccountKeyExpandRequest extends RPCRequest<AccountKeyPairResponse> {
+    
+    @Expose
+    @SerializedName("key")
+    private String privateKey;
+    
+    
+    public AccountKeyExpandRequest(String privateKey) {
+        super("key_expand", AccountKeyPairResponse.class);
+        this.privateKey = privateKey;
+    }
+    
+    
+    
+    public String getPrivateKey() {
+        return privateKey;
+    }
+    
+}
