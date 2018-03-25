df7053ce24cfeea29d95be4fc3ac60f30b62480f
Karl Oczadly <kocz@live.co.uk>
2018-03-25 03:22:49 +0100

    Add node add work peer request

---
+++ src/main/java/in/bigdolph/jnano/rpc/query/request/node/NodeAddWorkPeerRequest.java
@@ -0,0 +1,35 @@
+package in.bigdolph.jnano.rpc.query.request.node;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import in.bigdolph.jnano.rpc.query.request.RPCRequest;
+import in.bigdolph.jnano.rpc.query.response.RPCResponse;
+import in.bigdolph.jnano.rpc.query.response.specific.NodeVersionResponse;
+
+public class NodeAddWorkPeerRequest extends RPCRequest<RPCResponse> {
+    
+    @Expose
+    @SerializedName("address")
+    private String peerAddress;
+    
+    @Expose
+    @SerializedName("port")
+    private int peerPort;
+    
+    
+    public NodeAddWorkPeerRequest(String peerAddress, int peerPort) {
+        super("work_peer_add", RPCResponse.class);
+        this.peerAddress = peerAddress;
+        this.peerPort = peerPort;
+    }
+    
+    
+    public String getPeerAddress() {
+        return peerAddress;
+    }
+    
+    public int getPeerPort() {
+        return peerPort;
+    }
+    
+}
