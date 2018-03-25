87e04770eed0e55441e90aac6f2c73ee6be43b0f
Karl Oczadly <kocz@live.co.uk>
2018-03-25 02:47:54 +0100

    Add wallet change seed request

---
+++ src/main/java/in/bigdolph/jnano/rpc/query/request/wallet/WalletChangeSeedRequest.java
@@ -0,0 +1,36 @@
+package in.bigdolph.jnano.rpc.query.request.wallet;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import in.bigdolph.jnano.rpc.query.request.RPCRequest;
+import in.bigdolph.jnano.rpc.query.response.RPCResponse;
+import in.bigdolph.jnano.rpc.query.response.specific.NodeVersionResponse;
+
+public class WalletChangeSeedRequest extends RPCRequest<RPCResponse> {
+    
+    @Expose
+    @SerializedName("wallet")
+    private String walletId;
+    
+    @Expose
+    @SerializedName("seed")
+    private String seed;
+    
+    
+    public WalletChangeSeedRequest(String walletId, String seed) {
+        super("wallet_change_seed", RPCResponse.class);
+        this.walletId = walletId;
+        this.seed = seed;
+    }
+    
+    
+    
+    public String getWalletId() {
+        return walletId;
+    }
+    
+    public String getSeed() {
+        return seed;
+    }
+    
+}
