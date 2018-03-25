6f62ba15a5002d963fea5fab99df0bc9dbb5b938
Karl Oczadly <kocz@live.co.uk>
2018-03-25 03:19:34 +0100

    Add wallet locked check request

---
+++ src/main/java/in/bigdolph/jnano/rpc/query/request/wallet/WalletCheckLockRequest.java
@@ -0,0 +1,27 @@
+package in.bigdolph.jnano.rpc.query.request.wallet;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import in.bigdolph.jnano.rpc.query.request.RPCRequest;
+import in.bigdolph.jnano.rpc.query.response.specific.NodeVersionResponse;
+import in.bigdolph.jnano.rpc.query.response.specific.WalletLockedResponse;
+
+public class WalletCheckLockRequest extends RPCRequest<WalletLockedResponse> {
+    
+    @Expose
+    @SerializedName("wallet")
+    private String walletId;
+    
+    
+    public WalletCheckLockRequest(String walletId) {
+        super("wallet_locked", WalletLockedResponse.class);
+        this.walletId = walletId;
+    }
+    
+    
+    
+    public String getWalletId() {
+        return walletId;
+    }
+    
+}
