cb069f5f0ea6d3d63bd2f6b12902655261d0b3c2
Karl Oczadly <kocz@live.co.uk>
2018-03-25 03:16:29 +0100

    Added wallet unlock request

---
+++ src/main/java/in/bigdolph/jnano/rpc/query/request/wallet/WalletUnlockRequest.java
@@ -0,0 +1,36 @@
+package in.bigdolph.jnano.rpc.query.request.wallet;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import in.bigdolph.jnano.rpc.query.request.RPCRequest;
+import in.bigdolph.jnano.rpc.query.response.generic.ValidatedResponse;
+import in.bigdolph.jnano.rpc.query.response.specific.NodeVersionResponse;
+
+public class WalletUnlockRequest extends RPCRequest<ValidatedResponse> {
+    
+    @Expose
+    @SerializedName("wallet")
+    private String walletId;
+    
+    @Expose
+    @SerializedName("password")
+    private String password;
+    
+    
+    public WalletUnlockRequest(String walletId, String password) {
+        super("password_enter", ValidatedResponse.class);
+        this.walletId = walletId;
+        this.password = password;
+    }
+    
+    
+    
+    public String getWalletId() {
+        return walletId;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+    
+}
