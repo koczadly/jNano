a85f380c1239b581c8ea2eb7da02c3e5831677b0
Karl Oczadly <kocz@live.co.uk>
2018-03-25 00:57:54 +0000

    Add wallet set default representative request

---
+++ src/main/java/in/bigdolph/jnano/rpc/query/request/wallet/WalletSetRepresentativeRequest.java
@@ -0,0 +1,36 @@
+package in.bigdolph.jnano.rpc.query.request.wallet;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import in.bigdolph.jnano.rpc.query.request.RPCRequest;
+import in.bigdolph.jnano.rpc.query.response.RPCResponse;
+import in.bigdolph.jnano.rpc.query.response.specific.NodeVersionResponse;
+
+public class WalletSetRepresentativeRequest extends RPCRequest<RPCResponse> {
+    
+    @Expose
+    @SerializedName("wallet")
+    private String walletId;
+    
+    @Expose
+    @SerializedName("representative")
+    private String representativeAccount;
+    
+    
+    public WalletSetRepresentativeRequest(String walletId, String representativeAccount) {
+        super("wallet_representative_set", RPCResponse.class);
+        this.walletId = walletId;
+        this.representativeAccount = representativeAccount;
+    }
+    
+    
+    
+    public String getWalletId() {
+        return walletId;
+    }
+    
+    public String getRepresentativeAccount() {
+        return representativeAccount;
+    }
+    
+}
