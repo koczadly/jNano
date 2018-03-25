e664d2a14a3b8c0ec5890a5eb3c68fd5db9a90ae
Karl Oczadly <kocz@live.co.uk>
2018-03-25 03:10:24 +0100

    Add wallet fetch work request

---
+++ src/main/java/in/bigdolph/jnano/rpc/query/request/wallet/WalletFetchWorkRequest.java
@@ -0,0 +1,27 @@
+package in.bigdolph.jnano.rpc.query.request.wallet;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import in.bigdolph.jnano.rpc.query.request.RPCRequest;
+import in.bigdolph.jnano.rpc.query.response.specific.NodeVersionResponse;
+import in.bigdolph.jnano.rpc.query.response.specific.WalletFetchWorkResponse;
+
+public class WalletFetchWorkRequest extends RPCRequest<WalletFetchWorkResponse> {
+    
+    @Expose
+    @SerializedName("wallet")
+    private String walletId;
+    
+    
+    public WalletFetchWorkRequest(String walletId) {
+        super("wallet_work_get", WalletFetchWorkResponse.class);
+        this.walletId = walletId;
+    }
+    
+    
+    
+    public String getWalletId() {
+        return walletId;
+    }
+    
+}
