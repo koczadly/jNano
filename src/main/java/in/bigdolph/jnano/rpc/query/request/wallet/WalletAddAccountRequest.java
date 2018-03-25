036d97f6cd59b73a371c5e3902e95996b8351936
Karl Oczadly <kocz@live.co.uk>
2018-03-25 02:41:45 +0100

    Add wallet add account request

---
+++ src/main/java/in/bigdolph/jnano/rpc/query/request/wallet/WalletAddAccountRequest.java
@@ -0,0 +1,49 @@
+package in.bigdolph.jnano.rpc.query.request.wallet;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import in.bigdolph.jnano.rpc.query.request.RPCRequest;
+import in.bigdolph.jnano.rpc.query.response.generic.AccountResponse;
+import in.bigdolph.jnano.rpc.query.response.specific.NodeVersionResponse;
+
+public class WalletAddAccountRequest extends RPCRequest<AccountResponse> {
+    
+    @Expose
+    @SerializedName("wallet")
+    private String walletId;
+    
+    @Expose
+    @SerializedName("key")
+    private String key;
+    
+    @Expose
+    @SerializedName("work")
+    private boolean generateWork;
+    
+    
+    public WalletAddAccountRequest(String walletId, String key) {
+        this(walletId, key, true);
+    }
+    
+    public WalletAddAccountRequest(String walletId, String key, boolean generateWork) {
+        super("wallet_add", AccountResponse.class);
+        this.walletId = walletId;
+        this.key = key;
+        this.generateWork = generateWork;
+    }
+    
+    
+    
+    public String getWalletId() {
+        return walletId;
+    }
+    
+    public String getKey() {
+        return key;
+    }
+    
+    public boolean getGenerateWork() {
+        return generateWork;
+    }
+    
+}
