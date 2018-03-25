42b5b45d49618226bf75a165b4344ace2c9b41ab
Karl Oczadly <kocz@live.co.uk>
2018-03-25 03:22:10 +0100

    Add account work set request

---
+++ src/main/java/in/bigdolph/jnano/rpc/query/request/wallet/AccountWorkSetRequest.java
@@ -0,0 +1,45 @@
+package in.bigdolph.jnano.rpc.query.request.wallet;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import in.bigdolph.jnano.rpc.query.request.RPCRequest;
+import in.bigdolph.jnano.rpc.query.response.RPCResponse;
+import in.bigdolph.jnano.rpc.query.response.specific.NodeVersionResponse;
+
+public class AccountWorkSetRequest extends RPCRequest<RPCResponse> {
+    
+    @Expose
+    @SerializedName("wallet")
+    private String walletId;
+    
+    @Expose
+    @SerializedName("account")
+    private String account;
+    
+    @Expose
+    @SerializedName("work")
+    private String workSolution;
+    
+    
+    public AccountWorkSetRequest(String walletId, String account, String workSolution) {
+        super("work_set", RPCResponse.class);
+        this.walletId = walletId;
+        this.account = account;
+        this.workSolution = workSolution;
+    }
+    
+    
+    
+    public String getWalletId() {
+        return walletId;
+    }
+    
+    public String getAccount() {
+        return account;
+    }
+    
+    public String getWorkSolution() {
+        return workSolution;
+    }
+    
+}
