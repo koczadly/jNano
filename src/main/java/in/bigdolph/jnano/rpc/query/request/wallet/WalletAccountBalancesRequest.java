dd9389150901fe5318ef917f788eb9250bc1b23e
Karl Oczadly <kocz@live.co.uk>
2018-03-25 02:47:15 +0100

    Add wallet account balances request

---
+++ src/main/java/in/bigdolph/jnano/rpc/query/request/wallet/WalletAccountBalancesRequest.java
@@ -0,0 +1,42 @@
+package in.bigdolph.jnano.rpc.query.request.wallet;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import in.bigdolph.jnano.rpc.query.request.RPCRequest;
+import in.bigdolph.jnano.rpc.query.response.generic.BalancesResponse;
+import in.bigdolph.jnano.rpc.query.response.specific.NodeVersionResponse;
+
+import java.math.BigInteger;
+
+public class WalletAccountBalancesRequest extends RPCRequest<BalancesResponse> {
+    
+    @Expose
+    @SerializedName("wallet")
+    private String walletId;
+    
+    @Expose
+    @SerializedName("threshold")
+    private BigInteger threshold;
+    
+    
+    public WalletAccountBalancesRequest(String walletId) {
+        this(walletId, null);
+    }
+    
+    public WalletAccountBalancesRequest(String walletId, BigInteger threshold) {
+        super("wallet_balances", BalancesResponse.class);
+        this.walletId = walletId;
+        this.threshold = threshold;
+    }
+    
+    
+    
+    public String getWalletId() {
+        return walletId;
+    }
+    
+    public BigInteger getThreshold() {
+        return threshold;
+    }
+    
+}
