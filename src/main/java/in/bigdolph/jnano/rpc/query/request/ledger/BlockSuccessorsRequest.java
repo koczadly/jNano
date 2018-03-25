fb8bce33dea14ca56348951a6325409401852d2f
Karl Oczadly <kocz@live.co.uk>
2018-03-25 02:12:05 +0100

    Add block successors request

---
+++ src/main/java/in/bigdolph/jnano/rpc/query/request/ledger/BlockSuccessorsRequest.java
@@ -0,0 +1,36 @@
+package in.bigdolph.jnano.rpc.query.request.ledger;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import in.bigdolph.jnano.rpc.query.request.RPCRequest;
+import in.bigdolph.jnano.rpc.query.response.generic.BlockHashResponse;
+import in.bigdolph.jnano.rpc.query.response.specific.NodeVersionResponse;
+
+public class BlockSuccessorsRequest extends RPCRequest<BlockHashResponse> {
+    
+    @Expose
+    @SerializedName("hash")
+    private String blockHash;
+    
+    @Expose
+    @SerializedName("count")
+    private int count;
+    
+    
+    public BlockSuccessorsRequest(String blockHash, int count) {
+        super("successors", BlockHashResponse.class);
+        this.blockHash = blockHash;
+        this.count = count;
+    }
+    
+    
+    
+    public String getBlockHash() {
+        return blockHash;
+    }
+    
+    public int getCount() {
+        return count;
+    }
+    
+}
