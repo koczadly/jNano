d61a5977371724a153dafcd97476149714ffe5f4
Karl Oczadly <kocz@live.co.uk>
2018-03-25 02:42:41 +0100

    Add wallet total balance request

---
+++ src/main/java/in/bigdolph/jnano/rpc/query/request/wallet/WalletBalanceRequest.java
@@ -0,0 +1,27 @@
+package in.bigdolph.jnano.rpc.query.request.wallet;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import in.bigdolph.jnano.rpc.query.request.RPCRequest;
+import in.bigdolph.jnano.rpc.query.response.generic.BalanceResponse;
+import in.bigdolph.jnano.rpc.query.response.specific.NodeVersionResponse;
+
+public class WalletBalanceRequest extends RPCRequest<BalanceResponse> {
+    
+    @Expose
+    @SerializedName("wallet")
+    private String walletId;
+    
+    
+    public WalletBalanceRequest(String walletId) {
+        super("wallet_balance_total", BalanceResponse.class);
+        this.walletId = walletId;
+    }
+    
+    
+    
+    public String getWalletId() {
+        return walletId;
+    }
+    
+}
