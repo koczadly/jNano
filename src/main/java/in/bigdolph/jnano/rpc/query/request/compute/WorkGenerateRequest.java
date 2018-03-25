7ee94c6bb2d407f25251f0b61f5250d18026c694
Karl Oczadly <kocz@live.co.uk>
2018-03-25 03:21:09 +0100

    Add work generate request

---
+++ src/main/java/in/bigdolph/jnano/rpc/query/request/compute/WorkGenerateRequest.java
@@ -0,0 +1,27 @@
+package in.bigdolph.jnano.rpc.query.request.compute;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import in.bigdolph.jnano.rpc.query.request.RPCRequest;
+import in.bigdolph.jnano.rpc.query.response.generic.WorkResponse;
+import in.bigdolph.jnano.rpc.query.response.specific.NodeVersionResponse;
+
+public class WorkGenerateRequest extends RPCRequest<WorkResponse> {
+    
+    @Expose
+    @SerializedName("hash")
+    private String blockHash;
+    
+    
+    public WorkGenerateRequest(String blockHash) {
+        super("work_generate", WorkResponse.class);
+        this.blockHash = blockHash;
+    }
+    
+    
+    
+    public String getBlockHash() {
+        return blockHash;
+    }
+    
+}
---
+++ src/main/java/in/bigdolph/jnano/rpc/query/response/generic/WorkResponse.java
@@ -0,0 +1,19 @@
+package in.bigdolph.jnano.rpc.query.response.generic;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import in.bigdolph.jnano.rpc.query.response.RPCResponse;
+
+public class WorkResponse extends RPCResponse {
+
+    @Expose
+    @SerializedName("work")
+    private String workSolution;
+    
+    
+    
+    public String getWorkSolution() {
+        return workSolution;
+    }
+    
+}
