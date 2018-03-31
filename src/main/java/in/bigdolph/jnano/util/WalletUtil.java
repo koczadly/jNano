6e659ca1db7106e0c1cb2a8f76d9b2001f98571e
Karl Oczadly <kocz@live.co.uk>
2018-03-31 02:54:20 +0100

    Add base wallet utility class

---
+++ src/main/java/in/bigdolph/jnano/util/WalletUtil.java
@@ -0,0 +1,21 @@
+package in.bigdolph.jnano.util;
+
+import java.util.Random;
+
+public class WalletUtil {
+    
+    private static final char[] HEX_CHARS_UC = "0123456789ABCDEF".toCharArray();
+    private static final char[] HEX_CHARS_LC = "0123456789abcdef".toCharArray();
+    
+    
+    public static String generateNewSeed() {
+        return generateNewSeed(new Random());
+    }
+    
+    public static String generateNewSeed(Random random) {
+        StringBuilder seed = new StringBuilder();
+        for(int i=0; i<64; i++) seed.append(HEX_CHARS_UC[random.nextInt(16)]);
+        return seed.toString();
+    }
+    
+}
