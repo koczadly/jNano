d19dae52b214dde6faf83eb7d8fcad892d90d7e5
Karl Oczadly <kocz@live.co.uk>
2018-04-12 03:44:14 +0100

    Fixed returned HTTP response sent to node

--- src/main/java/in/bigdolph/jnano/rpc/callback/server/HttpClient.java
+++ src/main/java/in/bigdolph/jnano/rpc/callback/server/HttpClient.java
@@ -18,12 +18,12 @@ public class HttpClient implements Runnable {
     public void run() {
         try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(this.socket.getInputStream(), "utf-8"));
-    
+            
             String[] request = reader.readLine().split(" ");
             if(!request[0].equalsIgnoreCase("POST")) return; //Ignore non-post
-    
+            
             int contentLength = -1;
-    
+            
             String s;
             while((s = reader.readLine()) != null) {
                 if(s.equals("")) {
@@ -36,20 +36,21 @@ public class HttpClient implements Runnable {
                 }
             }
             if(contentLength == -1) return; //Couldn't read length
-    
+            
             //Read request body
             char[] buffer = new char[contentLength];
             reader.read(buffer, 0, contentLength);
-    
+            
             //Return OK
-            DataOutputStream output = new DataOutputStream(this.socket.getOutputStream());
-            output.writeUTF("HTTP/1.1 200 OK");
+            BufferedOutputStream output = new BufferedOutputStream(this.socket.getOutputStream());
+            output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes("UTF-8"));
             output.flush();
-    
+            output.close();
+            
             //Close socket
             this.socket.close();
-    
-            HttpRequest requestData = new HttpRequest(this.socket.getInetAddress(), request[1], contentLength, String.valueOf(buffer));
+            
+            HttpRequest requestData = new HttpRequest(this.socket.getInetAddress(), request.length > 2 ? request[1] : "", contentLength, String.valueOf(buffer));
             this.callbackServer.handleRequest(requestData);
         } catch (Exception e) {
             e.printStackTrace();
