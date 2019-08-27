f9ee8462f5664e9e021895eef1020a39d26155a7
Karl Oczadly <kocz@live.co.uk>
2019-08-27 17:58:22 +0100

    Update to java 9 + module support

---
+++ src/main/java/module-info.java
@@ -0,0 +1,21 @@
+module uk.oczadly.karl.jnano {
+    
+    //Models
+    exports uk.oczadly.karl.jnano.model.block;
+    //Callback
+    exports uk.oczadly.karl.jnano.callback;
+    //RPC
+    exports uk.oczadly.karl.jnano.rpc;
+    exports uk.oczadly.karl.jnano.rpc.exception;
+    exports uk.oczadly.karl.jnano.rpc.request;
+    exports uk.oczadly.karl.jnano.rpc.request.conversion;
+    exports uk.oczadly.karl.jnano.rpc.request.node;
+    exports uk.oczadly.karl.jnano.rpc.request.wallet;
+    exports uk.oczadly.karl.jnano.rpc.response;
+    //Utils
+    exports uk.oczadly.karl.jnano.util;
+    
+    //Dependencies
+    requires gson;
+
+}
