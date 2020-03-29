13cc8ad228d3f2c94e51d7105282b067afe5c111
Karl Oczadly <kocz@live.co.uk>
2020-03-29 21:35:01 +0100

    Add more javadoc implementation details for RpcResponse

--- src/main/java/uk/oczadly/karl/jnano/rpc/RpcResponse.java
+++ src/main/java/uk/oczadly/karl/jnano/rpc/RpcResponse.java
@@ -1,9 +1,15 @@
 package uk.oczadly.karl.jnano.rpc;
 
 import com.google.gson.JsonObject;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 
 /**
  * Represents a response from a Nano RPC query.
+ * <p>This class represents an RPC response, containing all of the structured data returned by the node.</p>
+ * <p>Classes which extend this class need to specify parameters as private fields, and MUST be marked with Gson's
+ * {@link Expose} annotation. The parameter name will be derived from the name of the field, unless specified otherwise
+ * using the {@link SerializedName} annotation.</p>
  */
 public abstract class RpcResponse {
     
