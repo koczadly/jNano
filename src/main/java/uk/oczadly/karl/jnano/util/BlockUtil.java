050f492062522108be4c1836e5f612f952d8a6f1
Karl Oczadly <kocz@live.co.uk>
2020-06-11 00:17:26 +0100

    Add epoch data constants

--- src/main/java/uk/oczadly/karl/jnano/util/BlockUtil.java
+++ src/main/java/uk/oczadly/karl/jnano/util/BlockUtil.java
@@ -8,4 +8,15 @@ public class BlockUtil {
      */
     public static final String GENESIS_BLOCK_HASH = "991CF190094C00F0B68E2E5F75F6BEE95A2E0BD93CEAA4A6734DB9F19B728948";
     
+    /**
+     * The {@code link} data value for V1 epoch blocks. This epoch represents the transition from legacy
+     * blocks (send/receive/open/change) to UTX state blocks.
+     */
+    public static final String EPOCH_IDENTIFIER_V1 = "65706F636820763120626C6F636B000000000000000000000000000000000000";
+    
+    /**
+     * The {@code link} data value for V2 epoch blocks. This epoch represents the change in required work difficulties.
+     */
+    public static final String EPOCH_IDENTIFIER_V2 = "65706F636820763220626C6F636B000000000000000000000000000000000000";
+    
 }
