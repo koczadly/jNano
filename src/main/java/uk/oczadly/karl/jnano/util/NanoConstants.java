665d7a4944e836ef89ff33b078460bd3ceb90f6a
Karl Oczadly <kocz@live.co.uk>
2020-06-12 03:20:34 +0100

    Create NanoConstants class for constant values

--- src/main/java/uk/oczadly/karl/jnano/util/NanoConstants.java
+++ src/main/java/uk/oczadly/karl/jnano/util/NanoConstants.java
@@ -1,12 +1,26 @@
 package uk.oczadly.karl.jnano.util;
 
-public class BlockUtil {
+public final class NanoConstants {
+    private NanoConstants() {}
+    
+    
+    /**
+     * The genesis account's address on the live Nano network.
+     */
+    public static final String ADDRESS_GENESIS_LIVE =
+            "nano_3t6k35gi95xu6tergt6p69ck76ogmitsa8mnijtpxm9fkcm736xtoncuohr3";
+    
+    /**
+     * The official designated burn address. Funds sent to this address will be permanently irretrievable.
+     */
+    public static final String ADDRESS_BURN = "nano_1111111111111111111111111111111111111111111111111111hifc8npp";
+    
     
     /**
      * The block hash of the genesis transaction on the live Nano network. This represents the block/transaction where
-     * all Nano units were introduced to the network (or "mined").
+     * all Nano units were initially introduced to the network.
      */
-    public static final String GENESIS_BLOCK_HASH = "991CF190094C00F0B68E2E5F75F6BEE95A2E0BD93CEAA4A6734DB9F19B728948";
+    public static final String BLOCK_HASH_GENESIS = "991CF190094C00F0B68E2E5F75F6BEE95A2E0BD93CEAA4A6734DB9F19B728948";
     
     /**
      * The {@code link} data value for V1 epoch blocks. This epoch represents the transition from legacy
@@ -18,5 +32,5 @@ public class BlockUtil {
      * The {@code link} data value for V2 epoch blocks. This epoch represents the change in required work difficulties.
      */
     public static final String EPOCH_IDENTIFIER_V2 = "65706F636820763220626C6F636B000000000000000000000000000000000000";
-    
+
 }
