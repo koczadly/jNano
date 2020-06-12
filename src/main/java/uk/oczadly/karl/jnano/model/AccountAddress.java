c9b117ffadf842ecfc131a3384bbb82ef0e4235e
Karl Oczadly <kocz@live.co.uk>
2020-06-12 20:33:54 +0100

    Add AccountAddress class (with unit tests) to represent accounts and provide native utility tools.

---
+++ src/main/java/uk/oczadly/karl/jnano/model/AccountAddress.java
@@ -0,0 +1,394 @@
+package uk.oczadly.karl.jnano.model;
+
+import com.google.gson.*;
+import com.google.gson.annotations.JsonAdapter;
+import com.rfksystems.blake2b.Blake2b;
+import uk.oczadly.karl.jnano.internal.JNanoHelper;
+import uk.oczadly.karl.jnano.internal.utils.BaseEncoder;
+
+import java.lang.reflect.Type;
+import java.util.Arrays;
+import java.util.Objects;
+
+/**
+ * <p>This class represents a Nano account address string. A wide range of native utilities are provided through the
+ * methods of this class, without needing to configure or connect to an external node.</p>
+ * <p>To instantiate this class, use the provided static methods (eg. {@link #parseAddress(String)}), or use the
+ * constructor to clone an existing object.</p>
+ */
+@JsonAdapter(AccountAddress.Adapter.class)
+public final class AccountAddress {
+    
+    /**
+     * The default Nano prefix, officially used on the live network. This value does not contain the additional
+     * separator char ({@link #PREFIX_SEPARATOR_CHAR}).
+     */
+    public static final String DEFAULT_PREFIX = "nano";
+    
+    /**
+     * The character which separates the prefix from the address string.
+     */
+    public static final char PREFIX_SEPARATOR_CHAR = '_';
+    
+    
+    private String prefix;
+    private final byte[] keyBytes;
+    
+    // Values below may be initialized lazily
+    private volatile byte[] checksumBytes;
+    private volatile String cachedAddress, publicKeyHex, segAddress, segChecksum;
+    
+    /**
+     * Clones an {@link AccountAddress} object.
+     * @param address the address to clone
+     */
+    public AccountAddress(AccountAddress address) {
+        this(address.prefix, address.keyBytes, address.checksumBytes, address.cachedAddress, address.publicKeyHex,
+                address.segAddress, address.segChecksum);
+    }
+    
+    private AccountAddress(String prefix, byte[] keyBytes, String segAddress, String publicKeyHex) {
+        this(prefix, keyBytes, null, null, publicKeyHex, segAddress, null);
+    }
+    
+    private AccountAddress(String prefix, byte[] keyBytes, byte[] checksumBytes, String cachedAddress,
+                           String publicKeyHex, String segAddress, String segChecksum) {
+        this.prefix = (prefix != null && !prefix.isEmpty()) ? prefix : null;
+        this.keyBytes = keyBytes;
+        this.checksumBytes = checksumBytes;
+        this.cachedAddress = cachedAddress;
+        this.publicKeyHex = publicKeyHex;
+        this.segAddress = segAddress;
+        this.segChecksum = segChecksum;
+    }
+    
+    
+    /**
+     * @return the prefix associated with this address (not including separator)
+     */
+    public String getPrefix() {
+        return prefix;
+    }
+    
+    /**
+     * Sets the prefix of this address.
+     * @param prefix the prefix to use, without the separator
+     */
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+        this.cachedAddress = null; // Clear cache
+    }
+    
+    /**
+     * @return an array of bytes which represent the public key of this address
+     */
+    public byte[] getPublicKeyBytes() {
+        return Arrays.copyOf(keyBytes, keyBytes.length);
+    }
+    
+    /**
+     * @return an array of bytes which represent the public key of this address
+     */
+    public byte[] getChecksumBytes() {
+        if (checksumBytes == null) {
+            synchronized (this) {
+                if (checksumBytes == null)
+                    checksumBytes = generateChecksumBytes(keyBytes);
+            }
+        }
+        return Arrays.copyOf(checksumBytes, checksumBytes.length);
+    }
+    
+    /**
+     * @return this address, represented by a 64-character hexadecimal string
+     */
+    public String getAsPublicKey() {
+        if (publicKeyHex == null) {
+            synchronized (this) {
+                if (publicKeyHex == null)
+                    publicKeyHex = JNanoHelper.ENCODER_HEX.encode(keyBytes);
+            }
+        }
+        return publicKeyHex;
+    }
+    
+    /**
+     * @return this address, complete with prefix and checksum
+     */
+    public synchronized String getAsAddress() {
+        if (cachedAddress == null) {
+            synchronized (this) {
+                if (cachedAddress == null) {
+                    // Generate
+                    StringBuilder sb = new StringBuilder(
+                            60 + (getPrefix() == null ? 0 : getPrefix().length() + 1)); // Capacity
+                    if (getPrefix() != null)
+                        sb.append(getPrefix()).append(PREFIX_SEPARATOR_CHAR);
+                    cachedAddress = sb.append(getAddressSegment()).append(getAddressChecksumSegment()).toString();
+                    return cachedAddress;
+                }
+            }
+        }
+        return cachedAddress;
+    }
+    
+    /**
+     * @return this address, without the prefix or checksum segments
+     */
+    public String getAddressSegment() {
+        if (segAddress == null) {
+            synchronized (this) {
+                if (segAddress == null)
+                    segAddress = JNanoHelper.ENCODER_NANO_B32.encode(getPublicKeyBytes());
+            }
+        }
+        return segAddress;
+    }
+    
+    /**
+     * @return the checksum segment of this address
+     */
+    public String getAddressChecksumSegment() {
+        if (segChecksum == null) {
+            synchronized (this) {
+                if (segChecksum == null)
+                    segChecksum = JNanoHelper.ENCODER_NANO_B32.encode(getChecksumBytes());
+            }
+        }
+        return segChecksum;
+    }
+    
+    
+    @Override
+    public String toString() {
+        return getAsAddress();
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AccountAddress that = (AccountAddress)o;
+        return Objects.equals(prefix, that.prefix) &&
+                Arrays.equals(keyBytes, that.keyBytes);
+    }
+    
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(prefix);
+        result = 31 * result + Arrays.hashCode(keyBytes);
+        return result;
+    }
+    
+    
+    /**
+     * Creates a new {@link AccountAddress} from a given address. The address should an encoded public key and checksum,
+     * along with an optional prefix (eg. {@code nano_34qjpc8t1u6wnb584pc4iwsukwa8jhrobpx4oea5gbaitnqafm6qsgoacpiz}).
+     * @param address the account address string
+     * @return the created address object
+     */
+    public static AccountAddress parseAddress(String address) {
+        if (address.length() < 60) throw new AccountAddressFormatException("Address string is too short.");
+        
+        int separatorIndex = address.indexOf(PREFIX_SEPARATOR_CHAR);
+        return parseAddressSegment(
+                address.substring(address.length() - 60, address.length() - 8),
+                separatorIndex <=0 ? null : address.substring(0, separatorIndex),
+                address.substring(address.length() - 8));
+    }
+    
+    /**
+     * Creates a new {@link AccountAddress} from a given address segment, using the default prefix. The segment
+     * excludes the initial prefix and checksum (the last 8 characters), (eg.
+     * {@code 34qjpc8t1u6wnb584pc4iwsukwa8jhrobpx4oea5gbaitnqafm6q}).
+     * @param address the account address segment
+     * @return the created address object
+     */
+    public static AccountAddress parseAddressSegment(String address) {
+        return parseAddressSegment(address, DEFAULT_PREFIX);
+    }
+    
+    /**
+     * Creates a new {@link AccountAddress} from a given address segment and prefix. The segment excludes the initial
+     * prefix and checksum (the last 8 characters), (eg. {@code 34qjpc8t1u6wnb584pc4iwsukwa8jhrobpx4oea5gbaitnqafm6q}).
+     * @param address the account address segment
+     * @param prefix  the protocol identifier prefix to use (without separator), or null for no prefix
+     * @return the created address object
+     */
+    public static AccountAddress parseAddressSegment(String address, String prefix) {
+        return parseAddressSegment(address, prefix, null);
+    }
+    
+    /**
+     * Creates a new {@link AccountAddress} from a given address segment and prefix, and validates the checksum. The
+     * segment excludes the initial prefix and checksum (the last 8 characters), (eg.
+     * {@code 34qjpc8t1u6wnb584pc4iwsukwa8jhrobpx4oea5gbaitnqafm6q}).
+     * @param address  the account address segment
+     * @param prefix   the protocol identifier prefix to use (without separator), or null for no prefix
+     * @param checksum the checksum to compare, or null for no comparison
+     * @return the created address object
+     * @throws AccountAddressFormatException if the address does not meet the required format criteria
+     */
+    public static AccountAddress parseAddressSegment(String address, String prefix, String checksum) {
+        if (address == null) throw new IllegalArgumentException("Address argument cannot be null.");
+        if (address.length() != 52)
+            throw new AccountAddressFormatException("Address string must be 52 characters long.");
+        if (checksum != null && checksum.length() != 8)
+            throw new AccountAddressFormatException("Expected checksum string must be 8 characters long.");
+        
+        // Preprocess
+        address = address.toLowerCase();
+        checksum = checksum != null ? checksum.toLowerCase() : null;
+        
+        // Create object
+        AccountAddress createdAddr = new AccountAddress(
+                prefix,
+                generateKeyBytes(address, JNanoHelper.ENCODER_NANO_B32),
+                address, null);
+        
+        // Verify checksum (if provided)
+        if (checksum != null && !checksum.equals(createdAddr.getAddressChecksumSegment()))
+            throw new AccountAddressFormatException("Provided checksum did not match the computed checksum.");
+        
+        return createdAddr;
+    }
+    
+    /**
+     * Creates a new {@link AccountAddress} from a given public key, using the default prefix. This consists of a
+     * 64-character hexadecimal string (eg. {@code 8AF1B28DA06C9CA2466159428733B971068BF154DBA2AB10372510D52E86CC97}).
+     * @param key the public key of the account, encoded in hexadecimal
+     * @return the created address object
+     * @throws AccountAddressFormatException if the address does not meet the required format criteria
+     */
+    public static AccountAddress parsePublicKey(String key) {
+        return parsePublicKey(key, DEFAULT_PREFIX);
+    }
+    
+    /**
+     * Creates a new {@link AccountAddress} from a given public key and prefix. This consists of a 64-character
+     * hexadecimal string (eg. {@code 8AF1B28DA06C9CA2466159428733B971068BF154DBA2AB10372510D52E86CC97}).
+     * @param key    the public key of the account, encoded in hexadecimal
+     * @param prefix the protocol identifier prefix to use (without separator), or null for no prefix
+     * @return the created address object
+     * @throws AccountAddressFormatException if the address does not meet the required format criteria
+     */
+    public static AccountAddress parsePublicKey(String key, String prefix) {
+        if (key == null) throw new IllegalArgumentException("Public key argument cannot be null.");
+        if (key.length() != 64) throw new AccountAddressFormatException("Key string must be 64 characters long.");
+        
+        // Preprocess
+        key = key.toUpperCase();
+        
+        // Create and return
+        return new AccountAddress(
+                prefix,
+                generateKeyBytes(key, JNanoHelper.ENCODER_HEX),
+                null, key);
+    }
+    
+    
+    /**
+     * Checks whether a given address string is valid. For an address to be considered valid, the format must be of
+     * an appropriate length, contain a prefix, and have a matching checksum value.
+     * @param address the account address string
+     * @return whether the given address string is valid
+     */
+    public static boolean isValid(String address) {
+        return isValid(address, null);
+    }
+    
+    /**
+     * Checks whether a given address string is valid. For an address to be considered valid, the format must be of
+     * an appropriate length, contain the defined prefix, and have a matching checksum value.
+     * @param address the account address string
+     * @param prefix  the prefix value to compare, or null to allow any
+     * @return whether the given address string is valid
+     */
+    public static boolean isValid(String address, String prefix) {
+        try {
+            AccountAddress addr = parseAddress(address);
+            return prefix == null || prefix.equalsIgnoreCase(addr.getPrefix());
+        } catch (AccountAddressFormatException e) {
+            return false;
+        }
+    }
+    
+    /**
+     * Compares the given address with the checksum component, and checks whether the address string is valid.
+     * @param addressSegment the account address segment
+     * @param checksum       the checksum segment
+     * @return whether the given address string is valid
+     */
+    public static boolean isSegmentValid(String addressSegment, String checksum) {
+        try {
+            parseAddressSegment(addressSegment, null, checksum);
+            return true;
+        } catch (AccountAddressFormatException e) {
+            return false;
+        }
+    }
+    
+    
+    /** Helper method to generate checksum bytes from a public key. */
+    private static byte[] generateChecksumBytes(byte[] keyBytes) {
+        //Digest
+        Blake2b digest = new Blake2b(null, 5, null, null); //Blake2b algorithm, 5 bytes length
+        digest.update(keyBytes, 0, keyBytes.length);
+        byte[] out = new byte[5];
+        digest.digest(out, 0);
+        
+        //Reverse byte array
+        byte[] rev = new byte[out.length];
+        for(int i=0; i<out.length; i++) {
+            rev[i] = out[out.length - i - 1];
+        }
+        return rev;
+    }
+    
+    private static byte[] generateKeyBytes(String str, BaseEncoder decoder) {
+        byte[] keyBytes;
+        try {
+            keyBytes = decoder.decode(str);
+        } catch (IllegalArgumentException e) { // Catch illegal characters
+            throw new AccountAddressFormatException(e);
+        }
+        if (keyBytes.length != 32)
+            throw new AccountAddressFormatException("Address/key bytes could not be decoded.");
+        return keyBytes;
+    }
+    
+    
+    
+    static class Adapter implements JsonSerializer<AccountAddress>, JsonDeserializer<AccountAddress> {
+        @Override
+        public AccountAddress deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+                throws JsonParseException {
+            String str = json.getAsString();
+            if (str.length() == 64 && str.indexOf(PREFIX_SEPARATOR_CHAR) == -1) {
+                return parsePublicKey(str); // Hex
+            } else {
+                return parseAddress(str); // Address
+            }
+        }
+        
+        @Override
+        public JsonElement serialize(AccountAddress src, Type typeOfSrc, JsonSerializationContext context) {
+            return new JsonPrimitive(src.getAsAddress());
+        }
+    }
+    
+    public static class AccountAddressFormatException extends IllegalArgumentException {
+        public AccountAddressFormatException() {
+            super();
+        }
+    
+        public AccountAddressFormatException(String msg) {
+            super(msg);
+        }
+    
+        public AccountAddressFormatException(Throwable cause) {
+            super(cause);
+        }
+    }
+
+}
---
+++ src/test/java/uk/oczadly/karl/jnano/model/AccountAddressTest.java
@@ -0,0 +1,106 @@
+package uk.oczadly.karl.jnano.model;
+
+import com.google.gson.JsonParser;
+import org.junit.Test;
+import uk.oczadly.karl.jnano.internal.JNanoHelper;
+
+import static org.junit.Assert.*;
+
+public class AccountAddressTest {
+    
+    static final String ACC_1_PREFIX = "ban";
+    static final String ACC_1_ADDR = "ban_38qjpc8t1u6wnb584pc4iwsukwa8jhrobpx4oea5gbaitnqafm6qwax3wk9c";
+    static final String ACC_1_ADDRSEG = "38qjpc8t1u6wnb584pc4iwsukwa8jhrobpx4oea5gbaitnqafm6q";
+    static final String ACC_1_CHECKSUM = "wax3wk9c";
+    static final String ACC_1_PUBKEY = "9AF1B28DA06C9CA2466159428733B971068BF154DBA2AB10372510D52E86CC97";
+    
+    static final String ACC_2_PREFIX = "nano";
+    static final String ACC_2_ADDR = "nano_34qjpc8t1u6wnb584pc4iwsukwa8jhrobpx4oea5gbaitnqafm6qsgoacpiz";
+    static final String ACC_2_PUBKEY = "8AF1B28DA06C9CA2466159428733B971068BF154DBA2AB10372510D52E86CC97";
+    
+    static final String INVALID_ADDR = "nano_38qjpc8t1u6wnb584pc4iwsukwa8jhrobpx4oea5gbaitnqafm6qwax3wk9a";
+    static final String INVALID_CHECKSUM = "wax3wk9a";
+    
+    
+    @Test
+    public void testParsing() {
+        // Test valid parses
+        assertEquals(ACC_1_ADDR,
+                AccountAddress.parsePublicKey(ACC_1_PUBKEY, ACC_1_PREFIX).getAsAddress());
+        
+        AccountAddress addr = AccountAddress.parseAddress(ACC_1_ADDR);
+        assertEquals(ACC_1_ADDR, addr.getAsAddress());
+        assertEquals(ACC_1_PREFIX, addr.getPrefix());
+    
+        assertEquals(ACC_1_PUBKEY,
+                AccountAddress.parseAddressSegment(ACC_1_ADDRSEG, ACC_1_PREFIX).getAsPublicKey());
+    
+        assertEquals(ACC_1_PUBKEY,
+                AccountAddress.parseAddressSegment(ACC_1_ADDRSEG, ACC_1_PREFIX, ACC_1_CHECKSUM)
+                        .getAsPublicKey());
+        
+        // Test invalid checksums
+        assertThrows(AccountAddress.AccountAddressFormatException.class,
+                () -> AccountAddress.parseAddress(INVALID_ADDR));
+        assertThrows(AccountAddress.AccountAddressFormatException.class,
+                () -> AccountAddress.parseAddressSegment(ACC_1_ADDRSEG, ACC_1_PREFIX, INVALID_CHECKSUM));
+    }
+    
+    @Test
+    public void testValidityChecks() {
+        assertTrue(AccountAddress.isValid(ACC_1_ADDR)); // No prefix specified
+        assertTrue(AccountAddress.isValid(ACC_1_ADDR, ACC_1_PREFIX)); // Right prefix
+        assertFalse(AccountAddress.isValid(ACC_1_ADDR, "nano")); // Wrong prefix
+        assertTrue(AccountAddress.isSegmentValid(ACC_1_ADDRSEG, ACC_1_CHECKSUM));
+        assertFalse(AccountAddress.isSegmentValid(ACC_1_ADDR, INVALID_CHECKSUM));
+    }
+    
+    @Test
+    public void testGetters() {
+        AccountAddress addr = AccountAddress.parsePublicKey(ACC_1_PUBKEY, ACC_1_PREFIX);
+        assertEquals(32, addr.getPublicKeyBytes().length);
+        assertEquals(5, addr.getChecksumBytes().length);
+        assertEquals(ACC_1_PREFIX, addr.getPrefix());
+        assertEquals(ACC_1_ADDRSEG, addr.getAddressSegment());
+        assertEquals(ACC_1_CHECKSUM, addr.getAddressChecksumSegment());
+        assertEquals(ACC_1_PUBKEY, addr.getAsPublicKey());
+        assertEquals(ACC_1_ADDR, addr.getAsAddress());
+    }
+    
+    @Test
+    public void testChangePrefix() {
+        AccountAddress addr = AccountAddress.parsePublicKey(ACC_1_PUBKEY, ACC_1_PREFIX);
+        assertEquals(ACC_1_PREFIX, addr.getPrefix());
+        assertEquals(ACC_1_ADDR, addr.getAsAddress());
+        addr.setPrefix("ban");
+        assertEquals("ban", addr.getPrefix());
+        assertTrue(addr.getAsAddress().startsWith("ban_"));
+    }
+    
+    @Test
+    public void testEquality() {
+        AccountAddress addr1 = AccountAddress.parsePublicKey(ACC_1_PUBKEY);
+        AccountAddress addr2 = AccountAddress.parsePublicKey(ACC_1_PUBKEY);
+        AccountAddress addr3 = AccountAddress.parsePublicKey(ACC_2_PUBKEY);
+    
+        assertEquals(addr1, addr2);
+        assertEquals(addr1.hashCode(), addr2.hashCode());
+        assertNotEquals(addr1, addr3);
+    }
+    
+    @Test
+    public void testJsonDeserializer() {
+        AccountAddress addr1 = JNanoHelper.GSON.fromJson("\"" + ACC_2_ADDR + "\"", AccountAddress.class);
+        AccountAddress addr2 = JNanoHelper.GSON.fromJson("\"" + ACC_2_PUBKEY + "\"", AccountAddress.class);
+        assertEquals(ACC_2_ADDR, addr1.getAsAddress());
+        assertEquals(ACC_2_ADDR, addr2.getAsAddress());
+    }
+    
+    @Test
+    public void testJsonSerializer() {
+        AccountAddress addr1 = AccountAddress.parseAddress(ACC_1_ADDR);
+        assertEquals(JsonParser.parseString("\"" + ACC_1_ADDR + "\""),
+                JNanoHelper.GSON.toJsonTree(addr1));
+    }
+    
+}
