a53efa9f10be1b766893a94ebc071b1febfcd1b5
Karl Oczadly <kocz@live.co.uk>
2020-06-12 16:33:25 +0100

    Add base encoding utility in preparation for local account parsing

--- src/main/java/uk/oczadly/karl/jnano/internal/JNanoHelper.java
+++ src/main/java/uk/oczadly/karl/jnano/internal/JNanoHelper.java
@@ -4,10 +4,14 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import uk.oczadly.karl.jnano.internal.gsonadapters.ArrayTypeAdapterFactoryFix;
 import uk.oczadly.karl.jnano.internal.gsonadapters.BooleanTypeDeserializer;
+import uk.oczadly.karl.jnano.internal.utils.BaseEncoder;
 
 public class JNanoHelper {
     
     public static final char[] HEX_CHARS_UC = "0123456789ABCDEF".toCharArray();
+    
+    public static final BaseEncoder ENCODER_HEX = new BaseEncoder(HEX_CHARS_UC);
+    public static final BaseEncoder ENCODER_NANO_B32 = new BaseEncoder("13456789abcdefghijkmnopqrstuwxyz");
     
     
     public static final Gson GSON = new GsonBuilder()
