47e0046fb67daf60d0242ccd8d3c6d5ef3b97687
Karl Oczadly <karl@oczadly.uk>
2020-06-26 01:53:18 +0100

    Add genesis block to NanoConstants

--- src/main/java/uk/oczadly/karl/jnano/util/NanoConstants.java
+++ src/main/java/uk/oczadly/karl/jnano/util/NanoConstants.java
@@ -1,6 +1,9 @@
 package uk.oczadly.karl.jnano.util;
 
 import uk.oczadly.karl.jnano.model.NanoAccount;
+import uk.oczadly.karl.jnano.model.block.Block;
+import uk.oczadly.karl.jnano.model.block.OpenBlock;
+import uk.oczadly.karl.jnano.model.work.WorkSolution;
 
 public final class NanoConstants {
     private NanoConstants() {}
@@ -19,10 +22,19 @@ public final class NanoConstants {
             NanoAccount.parseSegment("1111111111111111111111111111111111111111111111111111");
     
     
+    public static final Block BLOCK_GENESIS_LIVE = new OpenBlock(
+            "9F0C933C8ADE004D808EA1985FA746A7E95BA2A38F867640F53EC8F180BDFE9E2C1268DEAD7C2664F356E37ABA362BC" +
+                    "58E46DBA03E523A7B5A19E4B6EB12BB02",
+            new WorkSolution("62f05417dd3fb691"),
+            "E89208DD038FBB269987689621D52292AE9C35941A7484756ECCED92A65093BA",
+            ADDRESS_GENESIS_LIVE, ADDRESS_GENESIS_LIVE);
+    
     /**
      * The block hash of the genesis transaction on the live Nano network. This represents the block/transaction where
      * all Nano units were initially introduced to the network.
+     * @deprecated Use the {@link Block#getHash()} method of {@link #BLOCK_GENESIS_LIVE}.
      */
-    public static final String BLOCK_HASH_GENESIS = "991CF190094C00F0B68E2E5F75F6BEE95A2E0BD93CEAA4A6734DB9F19B728948";
-
+    @Deprecated(forRemoval = true)
+    public static final String BLOCK_HASH_GENESIS = BLOCK_GENESIS_LIVE.getHash();
+    
 }
