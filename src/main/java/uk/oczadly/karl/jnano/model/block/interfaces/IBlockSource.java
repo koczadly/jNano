e4675cd70e9177643308c4597640bd8871cdc3fa
Karl Oczadly <karl@oczadly.uk>
2020-07-24 13:58:07 +0100

    Remove redundant IBlockLink from IBlockSource interfaces.

--- src/main/java/uk/oczadly/karl/jnano/model/block/interfaces/IBlockSource.java
+++ src/main/java/uk/oczadly/karl/jnano/model/block/interfaces/IBlockSource.java
@@ -1,26 +1,13 @@
 package uk.oczadly.karl.jnano.model.block.interfaces;
 
-import uk.oczadly.karl.jnano.model.NanoAccount;
-
 /**
  * This interface is to be implemented by blocks which contain a source block hash.
  */
-public interface IBlockSource extends IBlockLink {
+public interface IBlockSource {
     
     /**
      * @return the hash of the source block which sent the funds
      */
     String getSourceBlockHash();
     
-    
-    @Override
-    default NanoAccount getLinkAsAccount() {
-        return NanoAccount.parsePublicKey(getSourceBlockHash());
-    }
-    
-    @Override
-    default String getLinkData() {
-        return getSourceBlockHash();
-    }
-
 }
