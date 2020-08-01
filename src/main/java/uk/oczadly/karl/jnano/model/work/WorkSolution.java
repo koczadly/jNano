e1572496958f329f5da610068796025920fe5eb2
Karl Oczadly <karl@oczadly.uk>
2020-08-01 15:34:24 +0100

    Update WorkSolution class to calculate difficulty and generate work locally.

--- src/main/java/uk/oczadly/karl/jnano/model/work/WorkSolution.java
+++ src/main/java/uk/oczadly/karl/jnano/model/work/WorkSolution.java
@@ -2,9 +2,13 @@ package uk.oczadly.karl.jnano.model.work;
 
 import com.google.gson.*;
 import com.google.gson.annotations.JsonAdapter;
+import com.rfksystems.blake2b.Blake2b;
+import uk.oczadly.karl.jnano.internal.JNanoHelper;
+import uk.oczadly.karl.jnano.rpc.request.node.RequestWorkGenerate;
 
 import java.lang.reflect.Type;
 import java.util.Objects;
+import java.util.Random;
 
 /**
  * This class represents a proof-of-work solution.
@@ -47,6 +51,34 @@ public class WorkSolution {
     }
     
     
+    /**
+     * Calculates the difficulty for a given root hash. The root value should be either the block hash for existing
+     * accounts, or the account's public key for the first block.
+     * @param root the root hash (64 character hex string)
+     * @return the difficulty of this work solution for the given root hash
+     */
+    public WorkDifficulty calculateDifficulty(String root) {
+        if (root == null) throw new IllegalArgumentException("Root argument cannot be null.");
+        if (!JNanoHelper.isValidHex(root, 64))
+            throw new IllegalArgumentException("Root argument must be a 64-character hex string.");
+        
+        return calculateDifficulty(JNanoHelper.ENCODER_HEX.decode(root));
+    }
+    
+    /**
+     * Calculates the difficulty for a given root hash. The root value should be either the block hash for existing
+     * accounts, or the account's public key for the first block.
+     * @param root the root hash (32 element byte array)
+     * @return the difficulty of this work solution for the given root hash
+     */
+    public WorkDifficulty calculateDifficulty(byte[] root) {
+        if (root == null) throw new IllegalArgumentException("Root array cannot be null.");
+        if (root.length != 32) throw new IllegalArgumentException("Root array must have a length of 32.");
+        
+        return new WorkDifficulty(toLong(calculateDifficulty(toByteArray(longVal), root)));
+    }
+    
+    
     @Override
     public String toString() {
         return getAsHexadecimal();
@@ -63,6 +95,88 @@ public class WorkSolution {
     @Override
     public int hashCode() {
         return Objects.hash(longVal);
+    }
+    
+    
+    /**
+     * <p>Generates a work solution from the given root and minimum difficulty threshold. The root value should be
+     * either the block hash for existing accounts, or the account's public key for the first block.</p>
+     * <p><strong>CAUTION:</strong> This method will generate the work on the CPU. For GPU calculations, use the
+     * work generation utility provided by the node through RPC
+     * ({@link RequestWorkGenerate}).</p>
+     * @param root      the root hash (64 character hex string)
+     * @param threshold the minimum difficulty threshold
+     * @return the generated work solution
+     */
+    public static WorkSolution generate(String root, WorkDifficulty threshold) {
+        if (root == null) throw new IllegalArgumentException("Root argument cannot be null.");
+        if (!JNanoHelper.isValidHex(root, 64))
+            throw new IllegalArgumentException("Root argument must be a 64-character hex string.");
+        
+        return generate(JNanoHelper.ENCODER_HEX.decode(root), threshold);
+    }
+    
+    /**
+     * <p>Generates a work solution from the given root and minimum difficulty threshold. The root value should be
+     * either the block hash for existing accounts, or the account's public key for the first block.</p>
+     * <p><strong>CAUTION:</strong> This method will generate the work on the CPU. For GPU calculations, use the
+     * work generation utility provided by the node through RPC
+     * ({@link RequestWorkGenerate}).</p>
+     * @param root      the root bytes (32 element byte array)
+     * @param threshold the minimum difficulty threshold
+     * @return the generated work solution
+     */
+    public static WorkSolution generate(byte[] root, WorkDifficulty threshold) {
+        if (root == null) throw new IllegalArgumentException("Root array cannot be null.");
+        if (root.length != 32) throw new IllegalArgumentException("Root array must have a length of 32.");
+        if (threshold == null) throw new IllegalArgumentException("Difficulty threshold cannot be null.");
+        
+        byte[] thresholdBytes = toByteArray(threshold.getAsLong());
+        byte[] work = new byte[8];
+        new Random(1).nextBytes(work); // Initialize byte array with random vals
+    
+        Blake2b digest = new Blake2b(null, 8, null, null);
+        byte[] difficulty = new byte[8];
+        
+        while (true) {
+            digest.reset();
+            digest.update(work, 0, work.length);
+            digest.update(root, 0, root.length);
+            digest.digest(difficulty, 0);
+            
+            // Compare threshold
+            boolean thresholdValid = true;
+            for (int i=0; i<thresholdBytes.length; i++) {
+                if (Byte.compareUnsigned(difficulty[i], thresholdBytes[i]) < 0) {
+                    thresholdValid = false;
+                    break;
+                }
+            }
+            if (thresholdValid)
+                return new WorkSolution(toLong(work));
+            
+            incrementArray(work);
+        }
+    }
+    
+    
+    private static void incrementArray(byte[] arr) {
+        for (int i=0; i<arr.length; i++) {
+            if (++arr[i] != 0) return;
+        }
+    }
+    
+    private static byte[] toByteArray(long val) {
+        return JNanoHelper.reverseArray(JNanoHelper.longToBytes(val));
+    }
+    
+    private static long toLong(byte[] val) {
+        return JNanoHelper.bytesToLong(JNanoHelper.reverseArray(val));
+    }
+    
+    private static byte[] calculateDifficulty(byte[] work, byte[] root) {
+        // NOTE: code is duplicated in generate() for better efficiency
+        return JNanoHelper.blake2b(8, work, root);
     }
     
     
