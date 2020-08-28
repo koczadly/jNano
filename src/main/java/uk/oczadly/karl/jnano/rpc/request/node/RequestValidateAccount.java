14dc59e2d4e851121c19c563b222f46027f7cb18
Karl Oczadly <karl@oczadly.uk>
2020-08-28 21:58:19 +0100

    Fix invalid javadoc.

--- src/main/java/uk/oczadly/karl/jnano/rpc/request/node/RequestValidateAccount.java
+++ src/main/java/uk/oczadly/karl/jnano/rpc/request/node/RequestValidateAccount.java
@@ -12,8 +12,8 @@ import uk.oczadly.karl.jnano.rpc.response.ResponseValidation;
  *
  * @see <a href="https://docs.nano.org/commands/rpc-protocol/#validate_account_number">Official RPC documentation</a>
  *
- * @deprecated Use of Java-based library method {@link NanoAccount#isValid(String)} is preferred for better efficiency
- * and less reliance on external services.
+ * @deprecated Use of Java-based library method {@link NanoAccount#isValid(String, String...)} (String)} is preferred
+ * for better efficiency and less reliance on external services.
  */
 @Deprecated
 public class RequestValidateAccount extends RpcRequest<ResponseValidation> {
