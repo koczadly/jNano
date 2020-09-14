5921fa17bf4b2a00878e4f4c4c5d42f377e85f65
Karl Oczadly <karl@oczadly.uk>
2020-09-14 19:36:24 +0100

    Add to block package javadocs.

--- src/main/java/uk/oczadly/karl/jnano/model/block/package-info.java
+++ src/main/java/uk/oczadly/karl/jnano/model/block/package-info.java
@@ -10,6 +10,15 @@
  * class, and implement any associated interfaces provided in the {@link uk.oczadly.karl.jnano.model.block.interfaces}
  * package.</p>
  *
+ * <p>The following block implementations are provided:</p>
+ * <ul>
+ *     <li>{@code open} - {@link uk.oczadly.karl.jnano.model.block.OpenBlock}</li>
+ *     <li>{@code send} - {@link uk.oczadly.karl.jnano.model.block.SendBlock}</li>
+ *     <li>{@code receive} - {@link uk.oczadly.karl.jnano.model.block.ReceiveBlock}</li>
+ *     <li>{@code change} - {@link uk.oczadly.karl.jnano.model.block.ChangeBlock}</li>
+ *     <li>{@code state} - {@link uk.oczadly.karl.jnano.model.block.StateBlock}</li>
+ * </ul>
+ *
  * @author Karl Oczadly
  */
 package uk.oczadly.karl.jnano.model.block;
