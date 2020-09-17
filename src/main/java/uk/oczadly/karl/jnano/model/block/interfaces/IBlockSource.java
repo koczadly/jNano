b19d6d755ab45bedc30a7c515d6396cce1d9c95e
Karl Oczadly <karl@oczadly.uk>
2020-09-17 17:16:57 +0100

    Remove IBlockLink from IBlockSource interface.

--- src/main/java/uk/oczadly/karl/jnano/model/block/interfaces/IBlockSource.java
+++ src/main/java/uk/oczadly/karl/jnano/model/block/interfaces/IBlockSource.java
@@ -5,12 +5,10 @@
 
 package uk.oczadly.karl.jnano.model.block.interfaces;
 
-import uk.oczadly.karl.jnano.model.NanoAccount;
-
 /**
  * This interface is to be implemented by blocks which contain a source block hash.
  */
-public interface IBlockSource extends IBlock, IBlockLink {
+public interface IBlockSource extends IBlock {
     
     /**
      * Returns the hash of the block which this block is interacting with (eg. a {@code receive} block accepting the
@@ -18,28 +16,5 @@ public interface IBlockSource extends IBlock, IBlockLink {
      * @return the hash of the source block
      */
     String getSourceBlockHash();
-    
-    
-    /**
-     * {@inheritDoc}
-     * @deprecated Implemented for compatibility with the link interface.
-     * @see #getSourceBlockHash()
-     */
-    @Deprecated
-    @Override
-    default String getLinkData() {
-        return getSourceBlockHash();
-    }
-    
-    /**
-     * {@inheritDoc}
-     * @deprecated Implemented for compatibility with the link interface.
-     * @see #getSourceBlockHash()
-     */
-    @Deprecated
-    @Override
-    default NanoAccount getLinkAsAccount() {
-        return NanoAccount.parsePublicKey(getSourceBlockHash());
-    }
     
 }
