cf0347b7789f27f7c3c5e80a4a9b46b0c888424d
Karl Oczadly <karl@oczadly.uk>
2020-09-27 19:34:44 +0100

    Remove unused values from ResponseWalletCreate.

--- src/main/java/uk/oczadly/karl/jnano/rpc/response/ResponseWalletCreate.java
+++ src/main/java/uk/oczadly/karl/jnano/rpc/response/ResponseWalletCreate.java
@@ -7,7 +7,6 @@ package uk.oczadly.karl.jnano.rpc.response;
 
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
-import uk.oczadly.karl.jnano.model.NanoAccount;
 
 /**
  * This response class contains a single wallet ID string.
@@ -17,32 +16,12 @@ public class ResponseWalletCreate extends RpcResponse {
     @Expose @SerializedName("wallet")
     private String walletId;
     
-    @Expose @SerializedName("last_restored_account")
-    private NanoAccount lastAccount;
-    
-    @Expose @SerializedName("restored_count")
-    private Integer restoredCount;
-    
     
     /**
      * @return the wallet's ID
      */
     public String getWalletId() {
         return walletId;
-    }
-    
-    /**
-     * @return the newest account imported to this wallet
-     */
-    public NanoAccount getLastAccount() {
-        return lastAccount;
-    }
-    
-    /**
-     * @return the number of accounts imported into this wallet
-     */
-    public int getRestoredCount() {
-        return restoredCount != null ? restoredCount : 0;
     }
     
 }
