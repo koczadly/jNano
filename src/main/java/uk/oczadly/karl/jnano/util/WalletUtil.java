2519dd01d053724415109a2055d3163efd55be92
Karl Oczadly <karl@oczadly.uk>
2021-01-18 22:56:27 +0000

    Change WalletUtil to use int instead of long for account index.

--- src/main/java/uk/oczadly/karl/jnano/util/WalletUtil.java
+++ src/main/java/uk/oczadly/karl/jnano/util/WalletUtil.java
@@ -14,8 +14,6 @@ import java.security.SecureRandom;
 public final class WalletUtil {
     private WalletUtil() {}
     
-    public static final long MAX_SEED_INDEX = 4294967295L;
-    
     
     /**
      * Generates and returns a new randomly created seed (or private key).
@@ -43,18 +41,19 @@ public final class WalletUtil {
      * Derives a private key from the given seed, using the zeroth index.
      * @param seed the seed, represented as a 64-character hex string
      * @return a 64-character hex value representing the private key
+     * @see #deriveKeyFromSeed(String, int) 
      */
     public static HexData deriveKeyFromSeed(String seed) {
-        return deriveKeyFromSeed(new HexData(seed), 0L);
+        return deriveKeyFromSeed(new HexData(seed), 0);
     }
     
     /**
      * Derives a private key from the given seed and index.
      * @param seed  the seed, represented as a 64-character hex string
-     * @param index the index of the account (from {@code 0} to {@code 4294967295})
+     * @param index the index of the account (as an unsigned integer)
      * @return a 64-character hex value representing the private key
      */
-    public static HexData deriveKeyFromSeed(String seed, long index) {
+    public static HexData deriveKeyFromSeed(String seed, int index) {
         return deriveKeyFromSeed(new HexData(seed), index);
     }
     
@@ -62,18 +61,19 @@ public final class WalletUtil {
      * Derives a private key from the given seed, using the zeroth index.
      * @param seed  the seed, represented as a 64-character hex object
      * @return a 64-character hex value representing the private key
+     * @see #deriveKeyFromSeed(HexData, int) 
      */
     public static HexData deriveKeyFromSeed(HexData seed) {
-        return deriveKeyFromSeed(seed, 0L);
+        return deriveKeyFromSeed(seed, 0);
     }
     
     /**
      * Derives a private key from the given seed and index.
      * @param seed  the seed, represented as a 64-character hex object
-     * @param index the index of the account (from {@code 0} to {@code 4294967295})
+     * @param index the index of the account (as an unsigned integer)
      * @return a 64-character hex value representing the private key
      */
-    public static HexData deriveKeyFromSeed(HexData seed, long index) {
+    public static HexData deriveKeyFromSeed(HexData seed, int index) {
         if (seed == null)
             throw new IllegalArgumentException("Seed cannot be null.");
         if (seed.length() != NanoConst.LEN_KEY_B)
@@ -86,19 +86,16 @@ public final class WalletUtil {
     /**
      * Derives a private key from the given seed and index.
      * @param seed  the seed, represented as a 32-element byte array
-     * @param index the index of the account (from {@code 0} to {@code 4294967295})
+     * @param index the index of the account (as an unsigned integer)
      * @return a 64-character hex value representing the private key
      */
-    public static byte[] deriveKeyFromSeed(byte[] seed, long index) {
+    public static byte[] deriveKeyFromSeed(byte[] seed, int index) {
         if (seed == null)
             throw new IllegalArgumentException("Seed cannot be null.");
         if (seed.length != NanoConst.LEN_KEY_B)
             throw new IllegalArgumentException("Seed length is invalid.");
-        if (index < 0 || index > MAX_SEED_INDEX)
-            throw new IllegalArgumentException("Account index is out of bounds.");
         
-        byte[] indexBytes = JNH.leftPadByteArray(JNH.longToBytes(index), 4, true);
-        return JNH.blake2b(32, seed, indexBytes);
+        return JNH.blake2b(32, seed, JNH.intToBytes(index));
     }
     
 }
--- src/test/java/uk/oczadly/karl/jnano/util/WalletUtilTest.java
+++ src/test/java/uk/oczadly/karl/jnano/util/WalletUtilTest.java
@@ -26,6 +26,8 @@ public class WalletUtilTest {
         HexData seed = new HexData("4F4648622C812DD194E0FAAB0A5BA2DFE6391E807FCDD814FE3FBB28A9919136");
         assertEquals(new HexData("A09DAA9C69FD79A940B51ADC04DF5C9F5693F8445B8B02EFA6A9C9DC650C586B"),
                 WalletUtil.deriveKeyFromSeed(seed, 24));
+        assertEquals(new HexData("FA91C5FFA80C6A99A4D5F8FB0928D69269546F43BB51FD335787848D0F740C52"),
+                WalletUtil.deriveKeyFromSeed(seed, -1));
     }
 
 }
