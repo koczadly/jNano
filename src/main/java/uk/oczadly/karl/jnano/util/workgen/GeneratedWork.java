f709521c7d6d85522d6292be8f4d7a6418334beb
Karl Oczadly <karl@oczadly.uk>
2021-02-12 12:52:05 +0000

    Fix and reword javadocs in GeneratedWork.

--- src/main/java/uk/oczadly/karl/jnano/util/workgen/GeneratedWork.java
+++ src/main/java/uk/oczadly/karl/jnano/util/workgen/GeneratedWork.java
@@ -13,6 +13,8 @@ import uk.oczadly.karl.jnano.util.workgen.policy.WorkDifficultyPolicy;
 
 /**
  * This class contains a generated work solution, and the originating request parameters.
+ *
+ * @see #getWork()
  */
 public final class GeneratedWork {
     
@@ -57,13 +59,9 @@ public final class GeneratedWork {
     }
     
     /**
-     * Returns the difficulty multiplier of the generated work solution, <em>in respect to the requested base
-     * difficulty</em>. The base difficulty in this instance is either the specified difficulty constant, or the
-     * difficulty provided by the {@link WorkDifficultyPolicy}).
-     *
-     * <p>This value factors in all difficulty multipliers; the original request multiplier (if specified through
-     * {@link WorkGenerator#generate(Block, double)}), and the recommended multiplier specified by the
-     * {@link WorkDifficultyPolicy#multiplier()}.</p>
+     * Returns the difficulty multiplier of the generated work solution, <em>in respect to the base difficulty</em>.
+     * The base difficulty in this instance is either the provided difficulty value in the request, or the
+     * difficulty provided by the {@link WorkDifficultyPolicy}.
      *
      * @return the difficulty multiplier in respect to the base difficulty
      */
@@ -73,9 +71,7 @@ public final class GeneratedWork {
     
     /**
      * Returns the root hash of the originating work request.
-     *
-     * <p>The value returned by this method also factors in the requested multiplier, if one was specified in the
-     * by invoking {@link WorkGenerator#generate(Block, double)}.</p>
+     * Note that this is <em>not</em> the hash of the block the work is being generated for.
      *
      * @return the requested work root
      */
@@ -85,7 +81,7 @@ public final class GeneratedWork {
     
     /**
      * Returns the requested minimum base difficulty threshold, not including any multipliers. If no difficulty was
-     * specified, then this would be provided by the {@link WorkDifficultyPolicy}.
+     * specified in the request, then this value would be provided by the {@link WorkDifficultyPolicy}.
      *
      * @return the requested base difficulty
      */
@@ -105,6 +101,10 @@ public final class GeneratedWork {
     
     /**
      * Returns the requested difficulty multiplier, <em>in respect to the base difficulty</em>.
+     *
+     * <p>This value factors in all difficulty multipliers; the original request multiplier (if specified through
+     * {@link WorkGenerator#generate(Block, double)}), and the recommended multiplier specified by the
+     * {@link WorkDifficultyPolicy#multiplier()}.</p>
      *
      * @return the requested difficulty multiplier
      */
