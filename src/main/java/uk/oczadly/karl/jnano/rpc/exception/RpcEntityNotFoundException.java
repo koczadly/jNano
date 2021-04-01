c3a389d9e042d95698ac1c73217a90eb26309445
Karl Oczadly <karl@oczadly.uk>
2021-04-01 17:03:48 +0100

    Change RpcEntityNotFoundException to extend RpcInvalidArgumentException.

--- src/main/java/uk/oczadly/karl/jnano/rpc/exception/RpcEntityNotFoundException.java
+++ src/main/java/uk/oczadly/karl/jnano/rpc/exception/RpcEntityNotFoundException.java
@@ -8,7 +8,7 @@ package uk.oczadly.karl.jnano.rpc.exception;
 /**
  * Thrown if one of the referenced entities (eg. account, wallet, block hash) do not exist.
  */
-public class RpcEntityNotFoundException extends RpcExternalException {
+public class RpcEntityNotFoundException extends RpcInvalidArgumentException {
     
     public RpcEntityNotFoundException(String rawMessage) {
         super(rawMessage);
