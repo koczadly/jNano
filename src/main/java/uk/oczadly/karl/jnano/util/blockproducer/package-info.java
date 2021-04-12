8dbe3f2eb8d122a9481b858abc448cefa22e9a0d
Karl Oczadly <karl@oczadly.uk>
2021-04-12 20:45:23 +0100

    Add package-info to blockproducer.

---
+++ src/main/java/uk/oczadly/karl/jnano/util/blockproducer/package-info.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021 Karl Oczadly (karl@oczadly.uk)
+ * Licensed under the MIT License
+ */
+
+/**
+ * This package provides a range of classes which can be used to create and sign blocks, providing a layer of
+ * abstraction to local block creation.
+ *
+ * @author Karl Oczadly
+ */
+package uk.oczadly.karl.jnano.util.blockproducer;
