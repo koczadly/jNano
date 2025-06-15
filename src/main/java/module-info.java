7ac3baa199ced9d213327fc58639578eaa891929
Karl Oczadly <karl@oczadly.uk>
2025-06-15 16:08:03 +0100

    Clean up module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,12 @@
 open module uk.oczadly.karl.jnano {
-    
+
+    // External dependencies
+    requires transitive com.google.gson;
+    requires blake2b;
+    requires net.i2p.crypto.eddsa;
+    requires Java.WebSocket;
+    requires jocl;
+
     // Models
     exports uk.oczadly.karl.jnano.model;
     exports uk.oczadly.karl.jnano.model.block;
@@ -7,8 +14,10 @@ open module uk.oczadly.karl.jnano {
     exports uk.oczadly.karl.jnano.model.block.factory;
     exports uk.oczadly.karl.jnano.model.work;
     exports uk.oczadly.karl.jnano.model.epoch;
+
     // Callback
     exports uk.oczadly.karl.jnano.callback;
+
     // RPC
     exports uk.oczadly.karl.jnano.rpc;
     exports uk.oczadly.karl.jnano.rpc.exception;
@@ -20,16 +29,10 @@ open module uk.oczadly.karl.jnano {
     exports uk.oczadly.karl.jnano.util.wallet;
     exports uk.oczadly.karl.jnano.util.workgen;
     exports uk.oczadly.karl.jnano.util.workgen.policy;
+
     // WebSocket
     exports uk.oczadly.karl.jnano.websocket;
     exports uk.oczadly.karl.jnano.websocket.topic;
     exports uk.oczadly.karl.jnano.websocket.topic.message;
-    
-    // Dependencies
-    requires transitive com.google.gson;
-    requires blake2b;
-    requires net.i2p.crypto.eddsa;
-    requires Java.WebSocket;
-    requires jocl;
 
 }
